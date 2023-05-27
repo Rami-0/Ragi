aa3ec536931395c084cee2f9cd83bc6b46cb85c6
Rami-0 <Ramysh11@icloud.com>
2023-05-27 19:51:30 +0600

    -

--- src/constants/Projects.js
+++ src/constants/Projects.js
@@ -21,14 +21,6 @@ export const myProjects = [
 		link: 'https://www.behance.net/gallery/146353781/TagPlus-Motion-Graphic',
 	},
 	{
-		name: 'korean',
-		img: korean,
-		id: 2,
-		boolean: false,
-		description: '',
-		link: 'https://www.behance.net/gallery/145216681/Korean-shed-Motion-Design',
-	},
-	{
 		name: 'masari',
 		img: masari,
 		id: 3,
@@ -37,29 +29,12 @@ export const myProjects = [
 		link: 'https://www.behance.net/gallery/145477191/Motion-Explainer-Video-Masari-',
 	},
 	{
-		name: 'LogoAnimation',
-		img: LogoAnimation,
-		id: 4,
-		boolean: false,
-		description: '',
-		link: 'https://www.behance.net/gallery/146381363/Logo-Animation-V1',
-	},
-	{
 		name: 'Giddam',
 		img: Qiddam,
 		id: 8,
 		boolean: false,
 		description: '',
 		link: 'https://instagram.com/p/Cll9Zalt1lG/',
-	},
-
-	{
-		name: 'seedAndSteam',
-		img: seedAndSteam,
-		id: 5,
-		boolean: false,
-		description: '',
-		link: 'https://www.behance.net/gallery/121650789/Seed-and-Stem',
 	},
 	{
 		name: 'KSAU-HS',
@@ -76,6 +51,32 @@ export const myProjects = [
 		boolean: false,
 		description: '',
 		link: 'https://www.instagram.com/reel/ClQZSVTDudW/?igshid=MzRlODBiNWFlZA==',
+	},
+
+	{
+		name: 'korean',
+		img: korean,
+		id: 2,
+		boolean: false,
+		description: '',
+		link: 'https://www.behance.net/gallery/145216681/Korean-shed-Motion-Design',
+	},
+	{
+		name: 'LogoAnimation',
+		img: LogoAnimation,
+		id: 4,
+		boolean: false,
+		description: '',
+		link: 'https://www.behance.net/gallery/146381363/Logo-Animation-V1',
+	},
+
+	{
+		name: 'seedAndSteam',
+		img: seedAndSteam,
+		id: 5,
+		boolean: false,
+		description: '',
+		link: 'https://www.behance.net/gallery/121650789/Seed-and-Stem',
 	},
 	// {
 	// 	name: 'AnimateHarmony',
